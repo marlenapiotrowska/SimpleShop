357633d370e7248a3bcf5c233dd8e74110afbae2
Marlena Piotrowska <marlena.piotrowska1996@gmail.com>
2025-04-12 09:58:41 +0200

    Init project

---
+++ SimpleShop.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SimpleShop.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
